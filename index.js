eaac0794d1708830c81167dd504b0a1be5a6df71
Borobudur-999 <anwarilwasis80@gmail.com>
2026-04-15 09:05:40 +0700

    Up-Tugas 1 dan 2

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let nama = "Rapi";
-let umur = 21;
+let nama = "Budi";
+let umur = 17;
 let aktif = true;
 console.log(nama, umur, aktif);
-function login(username, password) {
+function log(username, password) {
     return username === "admin" && password === "admin";
 }
-console.log(login("admin", "admin"));
+console.log(log("admin", "admin"));
+const use = {
+    id: 1,
+    nama: "Sinta",
+    role: "admin"
+};
+console.log(use);
+const buku1 = {
+    id: 1,
+    judul: "Belajar Backend"
+};
+console.log(buku1);
+const siswa = {
+    id: 20034,
+    nama: "Tapir",
+    kelas: "XI9"
+};
+console.log(siswa);
+function Umur(hitungUmur) {
+    const tahun = new Date().getFullYear();
+    const age = tahun - hitungUmur;
+    return age;
+}
+console.log(Umur(2009));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,11 +1,54 @@
-let nama: string = "Rapi";
-let umur: number = 21;
+let nama: string = "Budi";
+let umur: number = 17;
 let aktif: boolean = true;
 
 console.log(nama, umur, aktif);
 
-function login(username: string, password: string):
+function log(username: string, password: string):
 boolean {
 return username === "admin" && password === "admin";
 }
-console.log(login("admin", "admin"));
+console.log(log("admin", "admin"));
+
+interface User {
+    id: number;
+    nama: string;
+    role: string;
+}
+const use: User = {
+    id: 1,
+    nama: "Sinta",
+    role: "admin"
+};
+console.log(use);
+
+interface Buku {
+    id: number;
+    judul: string;
+    penulis?: string;
+}
+const buku1: Buku = {
+    id: 1,
+    judul: "Belajar Backend"
+};
+console.log(buku1);
+
+interface Siswa {
+    id: number;
+    nama: string;
+    kelas: string;
+}
+const siswa: Siswa = {
+    id: 20034,
+    nama: "Tapir",
+    kelas: "XI9"
+};
+console.log(siswa);
+
+function Umur(hitungUmur: number) : number {
+    const tahun = new Date().getFullYear();
+    const age = tahun - hitungUmur;
+
+    return age;
+}
+console.log(Umur(2009));
